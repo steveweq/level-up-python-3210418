fd072399cbfe3ec6bc8f9491d942339d6b22f01f
steveweq <159529494+steveweq@users.noreply.github.com>
2026-04-07 10:46:18 +0000

    adding my version of the solution

--- src/02 Identify a Palindrome/palindrome.py
+++ src/02 Identify a Palindrome/palindrome.py
@@ -1,12 +1,20 @@
 import re
+import string
 
 def is_palindrome(phrase):
     forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
     backwards = forwards[::-1]
     return forwards == backwards
 
+def is_palindrome_mine(str):
+    newStr = str.lower().replace(" ","").translate(str.maketrans("", "", string.punctuation))
+    result = newStr == newStr[::-1]
+    return result
 
 # commands used in solution video for reference
 if __name__ == '__main__':
     print(is_palindrome('hello world'))  # false
     print(is_palindrome("Go hang a salami, I'm a lasagna hog."))  # true
+    print(is_palindrome_mine("Abc C b a"))
+    print(is_palindrome_mine("Go hang a salami, I'm a lasagna hog."))  # true
+
